250e3c4707db7b729487ac7a9b73a06cb2a9c33d
mulcmu <mulcmu>
2023-12-14 14:07:48 -0500

    Updates for 8.3.11 build

--- Makefile
+++ Makefile
@@ -1,45 +1,45 @@
 #
 # Makefile
 #
-BIN = demo
+BIN = demo-8.3.11-arm
 DESTDIR = /usr
 PREFIX = /local
+LVGL_DIR = lvgl
 
-CC=gcc
-CXX=g++
+
+#CC=gcc
+CC=arm-linux-gnueabihf-gcc
 CFLAGS = -Wall -Wshadow -Wundef -Wmaybe-uninitialized
-CFLAGS += -O3 -g3 -I./
-
+CFLAGS += -O3 -g3 -I./ 
 # directory for local libs
-LDFLAGS = -L$(DESTDIR)$(PREFIX)/lib
+LDFLAGS = -L$(DESTDIR)$(PREFIX)/lib 
 LIBS += -lstdc++ -lm
 
 VPATH =
 
 $(info LDFLAGS ="$(LDFLAGS)")
 
-LVGL_DIR = lvgl
 
-include $(LVGL_DIR)/lvgl.mk
-include $(LVGL_DIR)/lv_drivers/lv_drivers.mk
-include $(LVGL_DIR)/lv_examples/lv_examples.mk
+CSRCS += $(shell find $(LVGL_DIR)/src -type f -name '*.c')
+CSRCS += $(shell find $(LVGL_DIR)/demos -type f -name '*.c')
+CSRCS += $(shell find $(LVGL_DIR)/examples -type f -name '*.c')
+CSRCS += $(shell find lv_drivers -type f -name '*.c')
+CSRCS += $(wildcard *.c)
 
 # folder for object files
-OBJDIR = ./obj
-
-CSRCS += $(wildcard *.c)
+OBJDIR = obj
 
 COBJS = $(patsubst %.c,$(OBJDIR)/%.o,$(CSRCS))
 
 SRCS = $(CSRCS)
 OBJS = $(COBJS)
 
-#.PHONY: clean
+
 
 all: default
 
 $(OBJDIR)/%.o: %.c
-	@mkdir -p $(OBJDIR)
+	@mkdir -p $(dir $@)
 	@$(CC)  $(CFLAGS) -c $< -o $@
 	@echo "CC $<"
 
--- demo.c
+++ demo.c
@@ -1,14 +1,14 @@
 #include "lvgl/lvgl.h"
-#include "lvgl/lv_drivers/display/fbdev.h"
-#include "lvgl/lv_drivers/indev/evdev.h"
-#include "lvgl/lv_examples/lv_examples.h"
+#include "lv_drivers/display/fbdev.h"
+#include "lv_drivers/indev/evdev.h"
+#include "lvgl/examples/lv_examples.h"
 #include <unistd.h>
 //#include <stdio.h>
 
 // display buffer size - not sure if this size is really needed
 #define LV_BUF_SIZE 384000		// 800x480
 // A static variable to store the display buffers
-static lv_disp_buf_t disp_buf;
+static lv_disp_draw_buf_t disp_buf;
 // Static buffer(s). The second buffer is optional
 static lv_color_t lvbuf1[LV_BUF_SIZE];
 static lv_color_t lvbuf2[LV_BUF_SIZE];
@@ -27,13 +27,15 @@ int main(void)
 	evdev_init();
 
 	// Initialize `disp_buf` with the display buffer(s)
-	lv_disp_buf_init(&disp_buf, lvbuf1, lvbuf2, LV_BUF_SIZE);
+	lv_disp_draw_buf_init(&disp_buf, lvbuf1, lvbuf2, LV_BUF_SIZE);
 
 	// Initialize and register a display driver
 	lv_disp_drv_t disp_drv;
 	lv_disp_drv_init(&disp_drv);
 	disp_drv.flush_cb = fbdev_flush;	// flushes the internal graphical buffer to the frame buffer
-	disp_drv.buffer = &disp_buf;		// set teh display buffere reference in the driver
+	disp_drv.draw_buf = &disp_buf;		// set the display buffere reference in the driver
+	disp_drv.hor_res = 800;
+	disp_drv.ver_res = 480;
 	lv_disp_drv_register(&disp_drv);
 
 	// Initialize and register a pointer device driver
